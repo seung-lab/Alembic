900676fc46e9b272c7f174cf26962907129b81c7
Thomas Macrina <tmacrina@princeton.edu>
2016-02-04 11:45:46 -0500

    Add whitespace to Match.jl

--- src/Match.jl
+++ src/Match.jl
@@ -22,24 +22,24 @@ function get_correspondence_patches(match::Match, ind)
 
 	# hack to support old properties
 	if !haskey(props, "full")
-	src_patch = h5read(src_path, "img", props["src_range"])
-	src_pt = props["src_pt_loc"]
-	dst_patch = h5read(dst_path, "img", props["dst_range"])
-	dst_pt = props["dst_pt_loc"]
+		src_patch = h5read(src_path, "img", props["src_range"])
+		src_pt = props["src_pt_loc"]
+		dst_patch = h5read(dst_path, "img", props["dst_range"])
+		dst_pt = props["dst_pt_loc"]
 	else
-	src_patch = h5read(src_path, "img", props["full"]["src_range"])
-	src_pt_loc = props["full"]["src_pt_loc"];
-	dst_patch = h5read(dst_path, "img", props["full"]["dst_range"])
-	dst_pt_loc = props["full"]["dst_pt_loc"];
-	scale = props["scale"];
-	if props["scale"] != 1
-	src_pt_loc = ceil(Int64, scale * src_pt_loc);
-	dst_pt_loc = ceil(Int64, scale * dst_pt_loc);
-	src_patch = imscale(src_patch, scale)[1]
-	dst_patch = imscale(dst_patch, scale)[1]
-	end
-	src_pt = src_pt_loc
-	dst_pt = dst_pt_loc
+		src_patch = h5read(src_path, "img", props["full"]["src_range"])
+		src_pt_loc = props["full"]["src_pt_loc"];
+		dst_patch = h5read(dst_path, "img", props["full"]["dst_range"])
+		dst_pt_loc = props["full"]["dst_pt_loc"];
+		scale = props["scale"];
+		if props["scale"] != 1
+			src_pt_loc = ceil(Int64, scale * src_pt_loc);
+			dst_pt_loc = ceil(Int64, scale * dst_pt_loc);
+			src_patch = imscale(src_patch, scale)[1]
+			dst_patch = imscale(dst_patch, scale)[1]
+		end
+		src_pt = src_pt_loc
+		dst_pt = dst_pt_loc
 	end
 
 	xc = normxcorr2(src_patch, dst_patch);
@@ -196,18 +196,7 @@ function get_properties(match::Match, property_name::String)
 	return map(get_property, match.correspondence_properties, repeated(property_name));
 end
 
-function filter!(match::Match; by = ENV["USER"], timestamp = string(now()), machine = gethostname(), filtertype = "manual", inds = [])
-	push!(match.filters, Dict{Any, Any}(
-				"by"	  => by,
-				"machine" => machine,
-				"timestamp" => timestamp,
-				"type"	  => filtertype,
-				"rejected"  => inds,
-			      ));
-	return length(inds);
-	end
-
-function filter_by_params!(match::Match, property_name, compare, threshold)
+function filter!(match::Match, property_name, compare, threshold)
 	attributes = map(get_property, match.correspondence_properties, repeated(property_name));
 	inds_to_filter = find(i -> compare(i, threshold), attributes);
 	push!(match.filters, Dict{Any, Any}(
