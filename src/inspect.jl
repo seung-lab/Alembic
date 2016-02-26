eb886198f813632858f87b90b312b58bd3e906bf
Thomas Macrina <tmacrina@princeton.edu>
2016-02-26 10:49:37 -0500

    Patch inspect_alignments by hardcoding the meshset indices

--- src/inspect.jl
+++ src/inspect.jl
@@ -25,9 +25,9 @@ end
 """
 The only function called by tracers to inspect alignment points
 """
-function inspect_alignments(firstindex, lastindex, meshset_ind)
+function inspect_alignments(meshset_ind)
   match_ind = 1
-  # firstindex, lastindex = (1,167,-3,-3), (2,149,-3,-3)
+  firstindex, lastindex = (2,149,-3,-3), (3,167,-3,-3)
   name = string(join(firstindex[1:2], ","),  "-", join(lastindex[1:2], ","),"_aligned")
   meshset = load_split(name, meshset_ind)
   match = meshset.matches[match_ind]
