dba3c3f07338a25b5a665beaf781fb89c455fbf1
Thomas Macrina <tmacrina@princeton.edu>
2016-03-25 13:24:14 -0400

    Include limited print function for filter evaluation

--- src/filters.jl
+++ src/filters.jl
@@ -95,3 +95,18 @@ function clear_all_filters!(indexA, indexB)
 		save(meshset) 
 	end
 end
+
+function print_filter_eval(indexA, indexB)
+	indices = get_index_range(indexA, indexB)
+	for ind in indices
+		meshset = load(ind) 
+		for (i, match) in enumerate(meshset.matches);
+            fp, fn, tp, total = eval_filters(match, [("sigma_5", >, 5, 0)] ,:)
+            p = (100 * tp / (fp + tp))
+            r = (100 * tp / (fn + tp))
+            if !isnan(r) && r < 100.0
+	            println(ind, " #", i, " ", p, " / ", r)
+            end
+       end
+   end
+end
