83e19d474704bbe96d1cef89c23c694f2daddca4
Thomas Macrina <tmacrina@princeton.edu>
2016-07-07 11:28:00 -0400

    Turn USE_PYPLOT back to a workstation dependent global

--- src/Julimaps.jl
+++ src/Julimaps.jl
@@ -59,7 +59,7 @@ if contains(gethostname(), "seunglab") || contains(gethostname(), "seungom")
   global const USE_PYPLOT = false;
 else
   #global const USE_PYPLOT = true;
-  global const USE_PYPLOT = false;
+  global const USE_PYPLOT = true;
 end
 
 
