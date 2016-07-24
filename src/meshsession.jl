27dec5ad7ad38fa7d0b0e0ce9ae55a47093c05c0
Thomas Macrina <tmacrina@princeton.edu>
2016-07-24 11:15:48 -0400

    Update premontage based on tile overlap
    * Use tile overlap based on premontage registry to update each
      tile's offset.
    * Start with fixed tile in the center of montage, then spiral
      outward to properly ensure simple tile fixing without need for
      dependency graph.
    * Include spiral_sort to sort a list of tile indices based on a
      center-to-perimeter spiral.
    * Parallelize overlap-based premontaging.
    * Include limited snap correction method, to lock a tile based on
      a given offset to its neighbors.
    * Include methods to get neighboring tile indices in Index.jl.

--- src/meshsession.jl
+++ src/meshsession.jl
@@ -1,13 +1,15 @@
 function montage(firstindex::Index, lastindex::Index)
   ind_range = get_index_range(premontaged(firstindex), premontaged(lastindex))
   ind_range = unique([montaged(i[1:2]...) for i in ind_range])
+  crop = [250,250]
   for index in ind_range
     ms = MeshSet(index)
-    if is_flagged(ms)
-      render_montaged(ms; render_full=false, render_review=true, flagged_only=true)
-    else
-      render_montaged(ms; render_full=true, render_review=false)
-    end
+    render_montaged(ms; render_full=true, render_review=false)
+    # if is_flagged(ms)
+    #   render_montaged(ms; render_full=false, render_review=true, flagged_only=true)
+    # else
+    #   render_montaged(ms; render_full=true, render_review=false)
+    # end
   end
 end
 
@@ -66,9 +68,9 @@ function prealign(firstindex::Index, lastindex::Index; fix_start=false)
     else 
       ms = prealign(index)
     end
-    if is_flagged(ms)
-      render_prealigned(index; render_full=false, render_review=true)
-    end
+    # if is_flagged(ms)
+    render_prealigned(index; render_full=false, render_review=true)
+    # end
   end
 end
 
@@ -246,73 +248,163 @@ function write_reviews_as_needed(firstindex::Index, lastindex::Index)
   end
 end
 
+function negative_nans(a)
+  return isnan(a) ? -Inf : a
+end
 
-#### ASSUMES HOMOGENEOUS TILE SIZE
-#function premontage(wafer_range::UnitRange{Int64})
-function premontage(wafer, start, tile_size = [8000,8000], expected_overlap = 0.145)
-#  for wafer in wafer_range
-    premontaged_path = get_path(premontaged(wafer, 1))
-    dir,name = splitdir(premontaged_path)
-    tiles = sort_dir(dir, ".h5");
-    tiles = filter(x->contains(x,"Tile"), tiles)
-    tile_indices = sort(map(parse_name, tiles))
-    tile_indices = filter(x->x[1] == wafer, tile_indices)
-    section_range = start:tile_indices[end][2]
-  for sec in section_range
-    premontaged_path = get_path(premontaged(wafer, sec))
-    dir,name = splitdir(premontaged_path)
-    println("$wafer, $sec")
-    tiles = sort_dir(dir, ".h5");
-    tiles = filter(x->contains(x,"Tile"), tiles)
-    tile_indices = sort(map(parse_name, tiles))
-    tile_indices = filter(x->x[1:2] == (wafer,sec), tile_indices)
+"""
+Create 2D integer spiral ordering from a midpoint
+http://stackoverflow.com/questions/3706219/algorithm-for-iterating-over-an-outward-spiral-on-a-discrete-2d-grid-from-the-or
 
-    if length(tile_indices) == 0 continue; end
-
-    fixed_indices = similar(tile_indices, 0)
-    images = map(get_image, tile_indices)
-    #images = map(Images.restrict, images);
-    #images = map(Images.restrict, images);
-    #images = map(Images.restrict, images);
-
-    #fix the first one
-    update_offset(tile_indices[1], [0, 0], tile_size);
-    push!(fixed_indices, tile_indices[1])
-    offsets = Points(length(tile_indices))
-    offsets[1] = [0,0];
-
-    sigma = [1,1]
-
-    for index in tile_indices[2:end]
-	target_index = fixed_indices[findfirst(this -> is_adjacent(index, this), fixed_indices)]
-	  cur_image = images[findfirst(ind -> ind == index, tile_indices)]
-	  target_image = images[findfirst(ind -> ind == target_index, tile_indices)]
-	if index[3] - target_index[3] == 1
-	  cur_patch = cur_image[1:round(Int64, tile_size[1] * expected_overlap), :]
-	  target_patch = target_image[(1 + round(Int64, tile_size[1] * (1 - expected_overlap))):end, :]
-	  expected_offset = [round(Int64, tile_size[1] * (1 - expected_overlap)), 0]
-	else
-	  cur_patch = cur_image[1:end, 1:round(Int64, tile_size[2] * expected_overlap)]
-	  target_patch = target_image[1:end, (1 + round(Int64, tile_size[2] * (1 - expected_overlap))):end]
-	  expected_offset = [0, round(Int64, tile_size[2] * (1 - expected_overlap))]
-	end
-	cur_patch = Images.restrict(cur_patch)
-	target_patch = Images.restrict(target_patch)
-	xc = normxcorr2(cur_patch, target_patch; shape="full")
-	xcd = xc - Images.imfilter_gaussian(xc, sigma);
-
-	rm, ind = findmax(xcd)
-	i_max, j_max = ind2sub(xcd, ind);
-	i_diff = 2 * (i_max - median(1:size(xcd, 1)))
-	j_diff = 2 * (j_max - median(1:size(xcd, 2)))
-
-#    	update_offset(index, [i_diff, j_diff] + get_offset(target_index), [size(images[findfirst(ind -> ind == index, tile_indices)])...]);
-    	update_offset(index, expected_offset + [i_diff, j_diff] + offsets[findfirst(ind -> ind == target_index, tile_indices)], tile_size);
-    	offsets[findfirst(ind -> ind == index, tile_indices)] = expected_offset + [i_diff, j_diff] + offsets[findfirst(ind -> ind == target_index, tile_indices)]
-    	push!(fixed_indices, index)
+If NUM_POINTS not specified, assumes: 
+* bounding box origin is [1,1]
+* bounding box is square off the maximum dimension
+* center is calculated with ceil (so, bounding width & height are even)
+"""
+function create_spiral(center::Array{Int64,1}, NUM_POINTS=(2*maximum(center))^2)
+  assert(*((center .> [0,0])...))
+  spiral = Array{Array{Int64,1}, 1}()
+  i, j = center
+  push!(spiral, center)
+  di, dj = [1, 0]
+  seg_length = 1
+  seg_passed = 0
+  for k in 1:NUM_POINTS-1
+    i += di
+    j += dj
+    push!(spiral, [i,j])
+    seg_passed += 1
+    if seg_passed == seg_length
+      seg_passed = 0
+      di, dj = -dj, di # 90 degree counterclockwise
+      if dj == 0
+        seg_length += 1
+      end
     end
-# end
- end
+  end
+  return spiral
+end
+
+function spiral_sort(indices)
+  rows = map(getindex, indices, repeated(3))
+  cols = map(getindex, indices, repeated(4))
+  row_range = maximum(rows) + minimum(rows)
+  col_range = maximum(cols) + minimum(cols)
+  center = [ceil(Int64, row_range/2), ceil(Int64, col_range/2)]
+  spiral = create_spiral(center)
+  section = indices[1][1:2]
+  spiral_indices = [(section..., s...) for s in spiral]
+  return filter(i->i in indices, spiral_indices)
+end
+
+function premontage(firstindex::Index, lastindex::Index)
+  scale = 0.5
+
+  indices = get_index_range(firstindex, lastindex)
+  sections = unique([ind[1:2] for ind in indices])
+
+  for sec in sections
+    println("Premontaging tiles in section $sec")
+    tile_indices = spiral_sort(get_index_range(premontaged(sec...), premontaged(sec...)))
+    bbs = map(get_bb, tile_indices)
+    overlaps = []
+    for k in 2:length(tile_indices)
+      mindex = tile_indices[k]
+      println("Finding overlap for $mindex")
+      moving_bb = bbs[k]
+      fixed_bbs = bbs[1:k-1]
+      overlap_area = [get_area(fixed_bb - moving_bb) for fixed_bb in fixed_bbs]
+      max_idx = selectperm(overlap_area, 1, rev=true, by=negative_nans)
+      if overlap_area[max_idx] > 100*100
+        findex = tile_indices[max_idx]
+        fixed_bb = fixed_bbs[max_idx]
+        push!(overlaps, (mindex, findex, moving_bb, fixed_bb, scale))
+      else
+        println("No overlap found")
+      end
+    end
+    
+    translations = pmap(find_translation, overlaps)
+    moving_indices = [i[1] for i in translations]
+    for index in tile_indices
+      k = findfirst(i -> i==index, moving_indices)
+      if k > 0
+        mindex, findex, moving_bb, fixed_bb, dv = translations[k]
+        offset = get_offset(findex) - get_offset(fixed_bb) + get_offset(mindex) + dv
+        update_offset(mindex, offset, get_image_size(mindex));
+      end
+    end
+    save_premontage_review(tile_indices[1])
+  end
+end
+
+function find_translation(overlap)
+  mindex, findex, moving_bb, fixed_bb, scale = overlap
+  dv = find_translation_offset(mindex, findex, moving_bb, fixed_bb, scale=scale)
+  return mindex, findex, moving_bb, fixed_bb, dv
+end
+
+function find_translation(moving_index::Index, fixed_index::Index, mbb=get_bb(moving_index), fbb=get_bb(fixed_index); scale=0.5)
+  println("Translating $moving_index to $fixed_index")
+  overlap_bb = get_bb(moving_index) - get_bb(fixed_index)
+  println("Overlap area: ", get_area(overlap_bb), " px^2")
+  moving = get_slice(moving_index, overlap_bb, scale, is_global=true)
+  fixed = get_slice(fixed_index, overlap_bb, scale, is_global=true) 
+  xc = normxcorr2(moving, fixed; shape="full")
+  return xc, moving, fixed
+end
+
+function find_translation_offset(moving_index::Index, fixed_index::Index, mbb=get_bb(moving_index), fbb=get_bb(fixed_index); scale=0.5)
+  xc, moving, fixed = find_translation(moving_index, fixed_index, mbb, fbb, scale=scale)
+  rm, ind = findmax(xc)
+  i_max, j_max = ind2sub(xc, ind);
+  i = 1/scale * (i_max - median(1:size(xc, 1)))
+  j = 1/scale * (j_max - median(1:size(xc, 2)))
+  return [i,j]
+end
+
+function get_major_overlaps(indices)
+  bbs = [i=>get_bb(i) for i in indices]
+  neighbors = map(get_cardinal_neighbors, indices)
+end
+
+function check_premontage(index::Index)
+  indices = get_index_range(premontaged(index), premontaged(index))
+end
+
+function set_relative_offset(moving_index::Index, fixed_index::Index, rel_offset)
+
+  offset = get_offset(fixed_index) + rel_offset
+  println("$moving_index, $fixed_index, $rel_offset")
+  # update_offset(moving_index, offset, get_image_size(moving_index))
+end
+
+function snap(index::Index, overlap=[880, 690])
+  h, w = get_image_size(index)
+  offset = get_offset(index)
+  vertical = 0
+  horizontal = 0
+  below = get_below(index)
+  if below != NO_INDEX
+    vertical = get_offset(below)[1] - (h-overlap[1])
+  else
+    above = get_above(index)
+    if above != NO_INDEX
+      vertical = get_offset(above)[1] + (h-overlap[1])
+    end
+  end
+  left = get_left(index)
+  if left != NO_INDEX
+    horizontal = get_offset(left)[2] + (w-overlap[2])
+  else
+    right = get_right(index)
+    if right != NO_INDEX
+      horizontal = get_offset(right)[2] - (w-overlap[2])
+    end
+  end
+  println("$index, $offset -> ($vertical, $horizontal)")
+  update_offset(index, [vertical, horizontal], (h,w))
+  save_premontage_review(index)
 end
 
 #### ASSUMES HOMOGENEOUS TILE SIZE
