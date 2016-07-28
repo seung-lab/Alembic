7a259c526815557cc441f9ab59b7833a0be1b960
Thomas Macrina <tmacrina@princeton.edu>
2016-07-28 16:26:47 -0400

    Update upload.jl with second batch of Pinky filepath

--- src/upload.jl
+++ src/upload.jl
@@ -1,9 +1,10 @@
 function upload_AIBS_google()
-	fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160720_google_cloud_upload.csv"
+	# fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160720_google_cloud_upload.csv" # FINISHED 160724
+	fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160726_google_cloud_upload.csv" # FINISHED 160728
 	meta = readdlm(fn, ',')
 	src_dir = "/media/tmacrina/"
 	dst_dir = "pinky"
-	for i in 2:size(meta,1)
+	for i in 1:size(meta,1)
 		src_path = joinpath(src_dir, meta[i,2], "*")
 		dst_path = joinpath("gs://", dst_dir, string(meta[i,1]), "")
 		run(`gsutil -m cp -r $src_path $dst_path`)
