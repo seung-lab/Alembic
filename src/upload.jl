f2ae2d3f2a2e716bc6fa3dc59d0a2572c4204a0c
Thomas Macrina <tmacrina@princeton.edu>
2016-08-03 13:59:41 -0400

    Update upload.jl with log of uploads to Google Cloud

--- src/upload.jl
+++ src/upload.jl
@@ -1,9 +1,11 @@
 function upload_AIBS_google()
-	# fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160720_google_cloud_upload.csv" # FINISHED 160724
-	fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160726_google_cloud_upload.csv" # FINISHED 160728
+	# fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160720_google_cloud_upload.csv" # FINISHED 160724 to allen-brain-institute
+	# fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160726_google_cloud_upload.csv" # FINISHED 160728 to allen-brain-institute
+	fn = "/media/tmacrina/667FB0797A5072D7/3D_align/mosaiced_images_160729_google_cloud_upload_seung_import.csv" # FINISHED 160731 to seung-import
 	meta = readdlm(fn, ',')
 	src_dir = "/media/tmacrina/"
-	dst_dir = "pinky"
+	# dst_dir = "pinky" # allen-brain-institute
+	dst_dir = "pinky_raw" # neuromancer-seung-import
 	for i in 1:size(meta,1)
 		src_path = joinpath(src_dir, meta[i,2], "*")
 		dst_path = joinpath("gs://", dst_dir, string(meta[i,1]), "")
