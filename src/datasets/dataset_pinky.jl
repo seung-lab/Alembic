d1a8256cd4b702ea88fdaede752d73ec66509a0b
dodamih <dodam.a.ih@gmail.com>
2016-12-24 06:52:30 +0000

    using pinky queue again

--- src/datasets/dataset_pinky.jl
+++ src/datasets/dataset_pinky.jl
@@ -19,14 +19,14 @@ global TASKS_POLL_FREQUENCY = 10;
 end
 
 if TASKS_LOCALE == "gcs"
-#=global TASKS_TASK_QUEUE_NAME = "task-queue-pinky";
+global TASKS_TASK_QUEUE_NAME = "task-queue-pinky";
 global TASKS_ERROR_QUEUE_NAME = "error-queue-pinky";
 global TASKS_DONE_QUEUE_NAME = "done-queue-pinky";
-global TASKS_REGISTRY_QUEUE_NAME = "registry-queue-pinky"; =#
-global TASKS_TASK_QUEUE_NAME = "task-queue-GCS";
+global TASKS_REGISTRY_QUEUE_NAME = "registry-queue-pinky";
+#=global TASKS_TASK_QUEUE_NAME = "task-queue-GCS";
 global TASKS_ERROR_QUEUE_NAME = "error-queue-GCS";
 global TASKS_DONE_QUEUE_NAME = "done-queue-GCS";
-global TASKS_REGISTRY_QUEUE_NAME = "registry-queue-GCS";
+global TASKS_REGISTRY_QUEUE_NAME = "registry-queue-GCS"; =#
 global TASKS_BUCKET_NAME = "seunglab_alembic";
 global TASKS_CACHE_DIRECTORY = BUCKET;
 global TASKS_BASE_DIRECTORY = DATASET;
