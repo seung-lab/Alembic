7982858999bbfeb4413c231cd8fafd4ae77aa305
dodamih <dodam.a.ih@gmail.com>
2017-02-22 22:42:58 +0000

    using SOA

--- src/params/params_zebrafish.jl
+++ src/params/params_zebrafish.jl
@@ -25,15 +25,15 @@ PREMATCH_PREALIGNMENT = false
 PREMATCH_TEMPLATE_RATIO_PREALIGNMENT = 0.35
 PREMATCH_SCALE_PREALIGNMENT = 0.03
 PREMATCH_ANGLES_PREALIGNMENT = 0
-#=
+
 #son of alignment
 MESH_LENGTH_ALIGNMENT = 100
 GLOBAL_OFFSETS_ALIGNMENT = true
 BLOCKMATCH_SCALE_ALIGNMENT = 1.0
-BLOCK_R_ALIGNMENT = 120
+BLOCK_R_ALIGNMENT = 180
 SEARCH_R_ALIGNMENT = 50
-=#
 
+#=
 MESH_LENGTH_ALIGNMENT = 375
 GLOBAL_OFFSETS_ALIGNMENT = true
 BLOCKMATCH_SCALE_ALIGNMENT = 0.25
@@ -42,6 +42,7 @@ BLOCK_R_ALIGNMENT = 800
 #SEARCH_R_ALIGNMENT = 300
 SEARCH_R_ALIGNMENT = 500
 #SEARCH_R_ALIGNMENT = 750
+=#
 
 PREMATCH_ALIGNMENT = false
 MESH_SPRING_COEFF_ALIGNMENT = 1.0
@@ -179,12 +180,12 @@ global PARAMS_ALIGNMENT = Dict(
 			     	"eta_newton" => ETA_NEWTON_ALIGNMENT,
 			     	"ftol_newton" => FTOL_NEWTON_ALIGNMENT),
 			     "filter" => Dict(
-#=			 # son of alignment
+			 # son of alignment
 					"dist" => (0,:get_properties,>,40,"norm"),
 			     		"sigma_filter_high" => (1,:get_properties, >, 2.0, 0.95),
 			     		"sigma_filter_mid" => (2,:get_properties, >, 4, 0.75),
-			     		"sigma_filter_low" => (3,:get_properties, >, 8, 0.50)=#
-			 
+			     		"sigma_filter_low" => (3,:get_properties, >, 8, 0.50)
+		#=	 
 			     		"sigma_filter_high" => (1,:get_properties, >, 8, 0.95),
 			     		"sigma_filter_mid" => (2,:get_properties, >, 90, 0.75),
 			     		"sigma_filter_low" => (3,:get_properties, >, 200, 0.50),
@@ -194,7 +195,7 @@ global PARAMS_ALIGNMENT = Dict(
 			     		"kurtosis_filter" => (5,:get_properties, >, 25, "src_kurtosis"),
 			     		"kurtosis_filter_edge" => (6,:get_properties, <, -1.90, "src_kurtosis"),
 					"centered_norm_filter" => (7,:get_centered_norms, >, 250),
-					"consensus" => (8,:get_normalized_norm_from_filtered_consensus, >, 4, 4000)
+					"consensus" => (8,:get_normalized_norm_from_filtered_consensus, >, 4, 4000) =#
 					
 					      ),
 			     "render" => Dict(
