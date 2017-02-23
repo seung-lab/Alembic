8fd361febcad40939810ac55d07075a95aa9c675
dodamih <dodam.a.ih@gmail.com>
2017-02-23 23:06:51 +0000

    increased acceptable distance

--- src/params/params_zebrafish.jl
+++ src/params/params_zebrafish.jl
@@ -181,7 +181,7 @@ global PARAMS_ALIGNMENT = Dict(
 			     	"ftol_newton" => FTOL_NEWTON_ALIGNMENT),
 			     "filter" => Dict(
 			 # son of alignment
-					"dist" => (0,:get_properties,>,40,"norm"),
+					"dist" => (0,:get_properties,>,60,"norm"),
 			     		"sigma_filter_high" => (1,:get_properties, >, 3.0, 0.95),
 			     		"sigma_filter_mid" => (2,:get_properties, >, 15, 0.75),
 			     		"sigma_filter_low" => (3,:get_properties, >, 30, 0.50)
