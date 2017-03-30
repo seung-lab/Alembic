fba39a6192eec088b369ebeaa6b77d55cb4950d9
dodamih <dodam.a.ih@gmail.com>
2017-03-30 22:46:47 +0000

    added fill value arg to call of fillpoly!

--- src/render/render.jl
+++ src/render/render.jl
@@ -18,7 +18,7 @@ end
 function apply_mask(index::Index)
 	img = get_image(index)
 	mask = load("mask", index)
-	fillpoly!(img, mask[:, 2], mask[:, 1]; reverse = true);
+	fillpoly!(img, Array{Int64,1}(mask[:, 2]), Array{Int64,1}(mask[:, 1]), zero(eltype(img)); reverse = true);
 	save(get_path(index), img);
 	return img
 end
