b95b25dc9d6416b31cf86f21889969d99c3a7b75
dodamih <dodam.a.ih@gmail.com>
2017-05-02 10:56:53 +0000

    father of alignment parameters

--- src/params/params_zebrafish.jl
+++ src/params/params_zebrafish.jl
@@ -26,15 +26,16 @@ PREMATCH_PREALIGNMENT = false
 PREMATCH_TEMPLATE_RATIO_PREALIGNMENT = 0.35
 PREMATCH_SCALE_PREALIGNMENT = 0.03
 PREMATCH_ANGLES_PREALIGNMENT = 0
-
+#=
 #son of alignment
 MESH_LENGTH_ALIGNMENT = 100
 GLOBAL_OFFSETS_ALIGNMENT = true
 BLOCKMATCH_SCALE_ALIGNMENT = 1.0
 BLOCK_R_ALIGNMENT = 300
 SEARCH_R_ALIGNMENT = 70 
+=#
 
-#=
+
 MESH_LENGTH_ALIGNMENT = 375
 GLOBAL_OFFSETS_ALIGNMENT = true
 BLOCKMATCH_SCALE_ALIGNMENT = 0.25
@@ -181,13 +182,15 @@ global PARAMS_ALIGNMENT = Dict(
 			     	"eta_newton" => ETA_NEWTON_ALIGNMENT,
 			     	"ftol_newton" => FTOL_NEWTON_ALIGNMENT),
 			     "filter" => Dict(
+			     #=
 			 # son of alignment
 					"dist" => (0,:get_properties,>,60,"norm"),
 			     		"sigma_filter_high" => (1,:get_properties, >, 3.0, 0.95),
 			     		"sigma_filter_mid" => (2,:get_properties, >, 15, 0.75),
 			     		"sigma_filter_low" => (3,:get_properties, >, 30, 0.50)
+					=#
 
-					#=
+					
 			 
 			     		"sigma_filter_high" => (1,:get_properties, >, 8, 0.95),
 			     		"sigma_filter_mid" => (2,:get_properties, >, 90, 0.75),
@@ -199,7 +202,7 @@ global PARAMS_ALIGNMENT = Dict(
 			     		"kurtosis_filter_edge" => (6,:get_properties, <, -1.90, "src_kurtosis"),
 					"centered_norm_filter" => (7,:get_centered_norms, >, 250),
 					"consensus" => (8,:get_normalized_norm_from_filtered_consensus, >, 4, 4000)
-					=#
+					
 					
 					      ),
 			     "render" => Dict(
