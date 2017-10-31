7869d0873093bbafe8c912a75c07c31c3e0485b0
Thomas Macrina <tmacrina@princeton.edu>
2017-10-31 17:41:36 -0400

    Clean up requirements, registered & unregistered

--- src/Alembic.jl
+++ src/Alembic.jl
@@ -1,96 +1,81 @@
 module Alembic
 
-PKGS_USED = ["HDF5", "JLD", "Images", "ImageView", "Colors", "FixedPointNumbers", "Cairo", "IterativeSolvers", "Optim", "Distributions", "RegERMs", "PyPlot", "SimpleTasks"]
+    # using OffsetArrays
+    using DataFrames
+    using FixedPointNumbers
+    using IterativeSolvers
+    using ImageRegistration
+    importall ImageRegistration
+    using Optim
+    using Distributions
+    using Compat
+    using StatsBase
+    using JSON
+    using Images
+    using CloudVolume
+    using Primes
+    if haskey(ENV, "MKLROOT")
+      using MKLSparse
+    end
 
-PKGS_USED_CLONABLE = ["https://github.com/JuliaSparse/MKLSparse.jl.git", 
-                      "https://github.com/seung-lab/ImageRegistration.git", 
-            		      "https://github.com/madeleineudell/ParallelSparseMatMul.jl.git",
-                      "https://github.com/seung-lab/CloudVolume.jl.git"
-                      ]
+    import Base.Iterators.repeated
 
-# using OffsetArrays
-using JLD
-using DataFrames
-# using Colors
-using FixedPointNumbers
-# using Base.Test
-# using Cairo
-using IterativeSolvers
-using ImageRegistration
-importall ImageRegistration
-using Optim
-using Distributions
-using Compat
-# using Graphics
-using StatsBase
-using JSON
-using Images
-# using SimpleTasks # Need to make SimpleTasks pass its tests
-using CloudVolume
-using Primes
-using PyCall
-if haskey(ENV, "MKLROOT")
-  using MKLSparse
-end
+    abstract type AbstractMesh 		end
+    abstract type AbstractMatch 		end
+    #abstract type AbstractProperties 	end
 
-import Base.Iterators.repeated
+    # Aliases for Points - a Point is a 1D array (with two elements), and a Points is a 2xN array where each column is a Point. 
+    #const Point{T <: Number} = Array{T,1};
+    #const Points{T <: Number} = Array{T,2};
+    Point{T <: Number} = Array{T,1};
+    Points{T <: Number} = Array{T,2};
 
-abstract type AbstractMesh 		end
-abstract type AbstractMatch 		end
-#abstract type AbstractProperties 	end
+    function columnviews{T}(pts::Points{T})
+    	@inbounds return map(view, repeated(pts), repeated(:), 1:size(pts, 2))
+    end
 
-# Aliases for Points - a Point is a 1D array (with two elements), and a Points is a 2xN array where each column is a Point. 
-#const Point{T <: Number} = Array{T,1};
-#const Points{T <: Number} = Array{T,2};
-Point{T <: Number} = Array{T,1};
-Points{T <: Number} = Array{T,2};
+    # sparse array for edges - columns represent edges and the rows represent the nodes
+    #const Edges{T <: Number} = SparseMatrixCSC{T, Int64}
+    Edges{T <: Number} = SparseMatrixCSC{T, Int64}
 
-function columnviews{T}(pts::Points{T})
-	@inbounds return map(view, repeated(pts), repeated(:), 1:size(pts, 2))
-end
+    FourTupleIndex = NTuple{4, Int64};    # (wafer, section, row, column)
+    FourTupleIndices = Tuple{FourTupleIndex, FourTupleIndex};    # (wafer, section, row, column)
 
-# sparse array for edges - columns represent edges and the rows represent the nodes
-#const Edges{T <: Number} = SparseMatrixCSC{T, Int64}
-Edges{T <: Number} = SparseMatrixCSC{T, Int64}
+    Triangle = NTuple{3, Int64};      # index of three points of the triangle for some point
+    Triangles = Array{Triangle, 1};       # index of three points of the triangle for some point
 
-FourTupleIndex = NTuple{4, Int64};    # (wafer, section, row, column)
-FourTupleIndices = Tuple{FourTupleIndex, FourTupleIndex};    # (wafer, section, row, column)
+    Weight = Tuple{Float64, Float64, Float64};    # weights for respective triangle
+    Weights = Array{Weight, 1};       # weights for respective triangle
 
-Triangle = NTuple{3, Int64};      # index of three points of the triangle for some point
-Triangles = Array{Triangle, 1};       # index of three points of the triangle for some point
+    Pairing = Tuple{Int64, Int64};        # useful for abstraction
+    Pairings = Array{Pairing, 1};       # useful for abstraction
 
-Weight = Tuple{Float64, Float64, Float64};    # weights for respective triangle
-Weights = Array{Weight, 1};       # weights for respective triangle
+    global const BinaryProperty = Array{Bool, 1};    	  # array of bools
+    global const FloatProperty = Array{Float64, 1}   	# array of floats
 
-Pairing = Tuple{Int64, Int64};        # useful for abstraction
-Pairings = Array{Pairing, 1};       # useful for abstraction
+    # global constants, independent of deployment
+    global const NO_MATCH = [0; 0; -1];
+    global const NO_TRIANGLE = (0, 0, 0);
+    global const NO_WEIGHTS = (0.0, 0.0, 0.0);
+    global const NO_POINT = [typemin(Int64), typemin(Int64)];
+    global const NO_RANGE = (0:0, 0:0);
 
-global const BinaryProperty = Array{Bool, 1};    	  # array of bools
-global const FloatProperty = Array{Float64, 1}   	# array of floats
+    global const eps = 1e-12;
+    global const eps_large = 1e-4;
+    global const eps_rec = 1 / eps;
 
-# global constants, independent of deployment
-global const NO_MATCH = [0; 0; -1];
-global const NO_TRIANGLE = (0, 0, 0);
-global const NO_WEIGHTS = (0.0, 0.0, 0.0);
-global const NO_POINT = [typemin(Int64), typemin(Int64)];
-global const NO_RANGE = (0:0, 0:0);
+    global const SPLIT_MESH_BASIS = 1000
 
-global const eps = 1e-12;
-global const eps_large = 1e-4;
-global const eps_rec = 1 / eps;
+    # blas_set_num_threads(4); #
 
-global const SPLIT_MESH_BASIS = 1000
+    include("core/include.jl")
+    include("math/include.jl")
+    include("utilities/author.jl")
+    include("utilities/utilities.jl")
 
-# blas_set_num_threads(4); #
-
-include("core/include.jl")
-include("math/include.jl")
-include("utilities/author.jl")
-include("utilities/utilities.jl")
-
-for s in filter(x->string(x)[1]!='#' && x!=:eval, names(Alembic,true))
-  # println("Exporting $s")
-  eval(Expr(:export, s))
-end
+    for s in filter(x->string(x)[1]!='#' && x!=:eval, names(Alembic,true))
+      # println("Exporting $s")
+      eval(Expr(:export, s))
+    end
 
 end
