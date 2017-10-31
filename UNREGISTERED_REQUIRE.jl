f79a281adbb0e361804eb85c0108d8395c4d2cee
Thomas Macrina <tmacrina@princeton.edu>
2017-10-31 18:29:33 -0400

    Update UNREGISTERED_REQUIRE

--- UNREGISTERED_REQUIRE.jl
+++ UNREGISTERED_REQUIRE.jl
@@ -1,9 +1,8 @@
-PKGS_USED_CLONABLE = ["https://github.com/JuliaSparse/MKLSparse.jl.git", 
-                      "https://github.com/seung-lab/ImageRegistration.git", 
+PKGS_USED_CLONABLE = ["https://github.com/seung-lab/ImageRegistration.git", 
         		      "https://github.com/madeleineudell/ParallelSparseMatMul.jl.git",
                       "https://github.com/seung-lab/CloudVolume.jl.git"
                       ]
 
 for pkg in PKGS_USED_CLONABLE
-	Pkg.add(pkg)
+	Pkg.clone(pkg)
 end
