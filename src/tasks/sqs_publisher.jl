55bcf7e55253bebdaa10104b803dd6c31d40fbd0
Thomas Macrina <tmacrina@princeton.edu>
2017-11-15 16:55:18 -0500

    Overload publish_messages to take in index pairs for matching

--- src/tasks/sqs_publisher.jl
+++ src/tasks/sqs_publisher.jl
@@ -24,4 +24,24 @@ function publish_messages(params_fn, queue_name)
     end
 end
 
-publish_messages(ARGS[1], ARGS[2])
+function publish_messages(params_fn, queue_name, pairs_fn)
+    secrets_subpath = ".cloudvolume/secrets/aws-secret.json"
+    secret_path = joinpath(homedir(), secrets_subpath)
+    aws_secrets = JSON.parsefile(secret_path)
+    env = aws_config(creds=AWSCredentials(collect(values(aws_secrets))...))
+    q = sqs_get_queue(env, queue_name)
+
+    pairs_arr = readdlm(pairs_fn, ',', Int)
+    pairs = [[pairs_arr[i,:]...] for i in 1:size(pairs_arr,1)]
+    params = JSON.parsefile(params_fn)
+
+    for (z_start, z_stop) in pairs
+        println("Publishing $((z_start, z_stop))")
+        p = deepcopy(params)
+        p["mesh"]["z_start"] = z_start
+        p["mesh"]["z_stop"] = z_stop
+        sqs_send_message(q, JSON.json(p))
+    end
+end
+
+publish_messages_from_file(ARGS...)
