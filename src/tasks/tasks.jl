823e40e32ec6861a82d6c169440e35580cf07bd3
Thomas Macrina <tmacrina@princeton.edu>
2017-11-16 00:51:29 -0500

    Update tasks, removing @everywhere for params

--- src/tasks/tasks.jl
+++ src/tasks/tasks.jl
@@ -7,7 +7,7 @@ function match_task(params::Dict)
 end
 
 function render_task(params::Dict)
-    @everywhere load_params(params)
+    load_params(params)
     ms = load("meshset", get_name())
     println("Render Task $(get_name(ms))")
     render(ms)
