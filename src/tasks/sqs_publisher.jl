6e7ab5698c5b3ff63665d0f133086d0ce6eaea0d
Thomas Macrina <tmacrina@princeton.edu>
2017-11-17 11:54:51 -0500

    Generalize sqs_publisher more

--- src/tasks/sqs_publisher.jl
+++ src/tasks/sqs_publisher.jl
@@ -4,17 +4,31 @@ using AWSCore
 using AWSSQS
 using JSON
 
-function publish_messages(params_fn, queue_name)
+function get_queue(queue_name)
     secrets_subpath = ".cloudvolume/secrets/aws-secret.json"
     secret_path = joinpath(homedir(), secrets_subpath)
     aws_secrets = JSON.parsefile(secret_path)
     env = aws_config(creds=AWSCredentials(collect(values(aws_secrets))...))
-    q = sqs_get_queue(env, queue_name)
+    return sqs_get_queue(env, queue_name)
+end
 
-    load_params_file(params_fn)
+function publish_message(params_fn::AbstractString, queue_name::AbstractString, 
+                                                    pairs_fn::AbstractString)
+    q = get_queue(queue_name)
+    load_params(params_fn)
+    params = JSON.parsefile(params_fn)
     pairs = get_all_overlaps(get_z_range(), PARAMS[:match][:depth], 
                                         symmetric=PARAMS[:match][:symmetric])
-    params = JSON.parsefile(params_fn)
+    if params_fn != ""
+        pairs_arr = readdlm(pairs_fn, ',', Int)
+        pairs = [[pairs_arr[i,:]...] for i in 1:size(pairs_arr,1)]
+    end
+
+    return publish_messages(q, params, pairs)
+end
+
+function publish_messages(queue_name::AbstractString, params::Dict, pairs::Array)
+    q = get_queue(queue_name)
     for (z_start, z_stop) in pairs
         println("Publishing $((z_start, z_stop))")
         p = deepcopy(params)
@@ -24,24 +38,6 @@ function publish_messages(params_fn, queue_name)
     end
 end
 
-function publish_messages(params_fn, queue_name, pairs_fn)
-    secrets_subpath = ".cloudvolume/secrets/aws-secret.json"
-    secret_path = joinpath(homedir(), secrets_subpath)
-    aws_secrets = JSON.parsefile(secret_path)
-    env = aws_config(creds=AWSCredentials(collect(values(aws_secrets))...))
-    q = sqs_get_queue(env, queue_name)
-
-    pairs_arr = readdlm(pairs_fn, ',', Int)
-    pairs = [[pairs_arr[i,:]...] for i in 1:size(pairs_arr,1)]
-    params = JSON.parsefile(params_fn)
-
-    for (z_start, z_stop) in pairs
-        println("Publishing $((z_start, z_stop))")
-        p = deepcopy(params)
-        p["mesh"]["z_start"] = z_start
-        p["mesh"]["z_stop"] = z_stop
-        sqs_send_message(q, JSON.json(p))
-    end
+if length(ARGS) > 0
+    publish_messages(ARGS...)
 end
-
-publish_messages(ARGS...)
