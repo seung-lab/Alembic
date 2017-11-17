58398d27c3457c436f44a1d3c0eaabe47589283f
root <you@example.com>
2017-11-17 16:05:15 +0000

    added a test client for timing test

---
+++ src/tasks/run_match_task.jl
@@ -0,0 +1,25 @@
+using JSON
+include("tasks.jl")
+
+param_file = ARGS[1]
+
+n = 1
+if length(ARGS) > 1
+    n = parse(ARGS[2])
+end
+
+println("Importing Alembic...")
+@time begin
+    if n > 1
+        addprocs(n);
+        @everywhere using Alembic
+    else
+        using Alembic
+    end
+end
+
+params = JSON.parsefile(param_file)
+println("Runnign match task...")
+@time begin
+   match_task(params)
+end
