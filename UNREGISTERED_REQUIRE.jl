477076ffc619dd04b9b6994e67694d028f1ebb13
Thomas Macrina <tmacrina@princeton.edu>
2017-11-30 19:25:14 -0500

    Remove ParallelSparseMatMul requirement

--- UNREGISTERED_REQUIRE.jl
+++ UNREGISTERED_REQUIRE.jl
@@ -1,5 +1,4 @@
 PKGS_USED_CLONABLE = ["https://github.com/seung-lab/ImageRegistration.git", 
-        		      "https://github.com/madeleineudell/ParallelSparseMatMul.jl.git",
                       "https://github.com/seung-lab/CloudVolume.jl.git"
                       ]
 
