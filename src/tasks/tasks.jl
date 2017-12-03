2a323fb01ca7d9878b7af73331829220acb330a1
Thomas Macrina <thomas.macrina@gmail.com>
2017-12-03 13:05:24 -0500

    Remove commented lines from tasks.jl

--- src/tasks/tasks.jl
+++ src/tasks/tasks.jl
@@ -6,20 +6,12 @@ function match_task(params::Dict)
     split_meshset(ms)
 end
 
-"""
-Fix the center section, solve in halves, then resolve in range around center
-"""
-# function solve_task(params::Dict)
-# end
-
 function render_task(params::Dict)
     load_params(params)
     for z in get_z_range()
         ms = load("mesh", string(z))
         println("Render Task $(get_name(ms))")
         render(ms)
-        # reset_cache()
     end
-    # flush_cv_cache("src_image")
 end
 
