b5c09e118e47dec566a6fa5c611145fd225511f7
Thomas Macrina <tmacrina@princeton.edu>
2018-01-04 11:50:20 -0500

    Update load call in tasks

--- src/tasks/tasks.jl
+++ src/tasks/tasks.jl
@@ -9,7 +9,7 @@ end
 function render_task(params::Dict)
     load_params(params)
     for z in get_z_range()
-        ms = load("mesh", string(z))
+        ms = load(:mesh, string(z))
         println("Render Task $(get_name(ms))")
         render(ms)
     end
