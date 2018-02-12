5d28ebf57acb774a16ace6e97f27998054eb718d
Thomas Macrina <tmacrina@princeton.edu>
2018-02-11 21:31:39 -0500

    Rework how AWSSDK is installed

--- UNREGISTERED_REQUIRE.jl
+++ UNREGISTERED_REQUIRE.jl
@@ -1,7 +1,8 @@
 PKGS_USED_CLONABLE = ["https://github.com/seung-lab/ImageRegistration.git", 
                       "https://github.com/seung-lab/CloudVolume.jl.git",
                       "https://github.com/jingpengw/AWSCore.jl.git",
-                      "https://github.com/jingpengw/AWSSQS.jl"
+                      "https://github.com/jingpengw/AWSSDK.jl.git",
+                      "https://github.com/jingpengw/AWSSQS.jl.git"
                       ]
 
 for pkg in PKGS_USED_CLONABLE
