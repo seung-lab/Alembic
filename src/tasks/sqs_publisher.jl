ed6cbdfe8eedb23139dbabb5b0c67787ddb1f717
Thomas Macrina <tmacrina@princeton.edu>
2018-02-11 21:26:40 -0500

    Update publisher with AWS hacks & require AWSSDK

--- src/tasks/sqs_publisher.jl
+++ src/tasks/sqs_publisher.jl
@@ -5,11 +5,22 @@ using AWSSQS
 using JSON
 
 function get_queue(queue_name)
+    # secrets_subpath = ".cloudvolume/secrets/aws-secret.json"
+    # secret_path = joinpath(homedir(), secrets_subpath)
+    # aws_secrets = JSON.parsefile(secret_path)
+    # env = aws_config(creds=AWSCredentials(collect(values(aws_secrets))...))
+    # return sqs_get_queue(env, queue_name)
     secrets_subpath = ".cloudvolume/secrets/aws-secret.json"
     secret_path = joinpath(homedir(), secrets_subpath)
     aws_secrets = JSON.parsefile(secret_path)
     env = aws_config(creds=AWSCredentials(collect(values(aws_secrets))...))
-    return sqs_get_queue(env, queue_name)
+    # q = sqs_get_queue(env, queue_name)
+    # This is a hack, using https://github.com/jingpengw/AWSCore.jl
+    # + expanding the sqs_get_queue function. Should fork AWSCore
+    # for Alembic.
+    r = AWSSQS.sqs(env, "GetQueueUrl", QueueName = queue_name)
+    url = r["QueueUrl"]
+    return merge(env, Dict(:resource => HTTP.URI(url).path))
 end
 
 function publish_message(params_fn::AbstractString, queue_name::AbstractString, 
